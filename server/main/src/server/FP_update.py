25533760c4bc1044dcf4623783d0654b643f346d
Nima <naa51@sfu.ca>
2023-02-21 18:21:14 -0800

    test python file 9

--- server/main/src/server/FP_update.py
+++ server/main/src/server/FP_update.py
@@ -24,3 +24,22 @@ def show_floorplan(floor_level, device_num, location):
 # device_num = 12
 # location = 7
 # show_floorplan(floor_level, device_num, location)
+
+
+# this part for test
+
+floor_str = "/static/img2/org_floorplan1.png"
+img = Image.open(floor_str)
+img_array = plt.imread(floor_str)
+fig, ax = plt.subplots()
+ax.imshow(img_array)
+#     real access points
+#     access_points= [(0,0),(0,0.9), (0, 1.8), (0,2.7), 
+#                     (0.9, 2.7), (0.9, 1.8), (0.9, 0.9), (0.9, 0)]
+#   (0.0) = (450, 180)
+access_points = [(450,180),(450,270), (450, 360), (450,450), 
+                (630, 450), (630, 360), (630, 270), (630, 180)]
+position = access_points[4]
+ax.add_patch(plt.Circle(position, radius=10, color='r'))
+ax.annotate(f"Device device122", position, color='r')
+fig.savefig('/static/img2/floorplan1.png')
