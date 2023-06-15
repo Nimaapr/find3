3478850d490618b0056490d4119c26e5d9f5b78f
Nima <naa51@sfu.ca>
2023-06-14 20:20:10 -0700

    Added CSV database

--- server/main/src/server/Eq_process.py
+++ server/main/src/server/Eq_process.py
@@ -1,6 +1,7 @@
 import sys
 import json
-import sqlite3
+import csv
+# import sqlite3
 
 
 family = sys.argv[1]
@@ -9,31 +10,41 @@ timestamp = sys.argv[3]
 device = sys.argv[4]
 location = sys.argv[5]
 
-# Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('Eq_beacons.db')
-c = conn.cursor()
+with open('/app/main/static/img2/eq_process.txt', 'w') as f:
+        f.write(family + "\n")
+        f.write(sensors + "\n")
+        f.write(timestamp + "\n")
+        f.write(device + "\n")
+        f.write(location + "\n")
 
-# Create table (if it doesn't exist)
-c.execute('''CREATE TABLE IF NOT EXISTS Eq_beacons 
-             (timestamp TEXT, family TEXT, device TEXT, location TEXT, 
-              beacon TEXT, value INTEGER)''')
+
+# Set CSV filename
+csv_filename = '/app/main/static/img2/Eq_beacons.csv'
 
 def process_data(data):
     # Extract the bluetooth data
     bluetooth_data = data["bluetooth"]
+    
+    modified_bluetooth = {}
+    for key, value in bluetooth_data.items():
+        if key.startswith("St"):
+            modified_bluetooth[key] = value
+        elif key.startswith("Eq"):
+            # Open CSV file and append data
+            with open(csv_filename, 'a', newline='') as csvfile:
+                fieldnames = ['timestamp', 'family', 'device', 'location', 'key', 'value']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    # modified_bluetooth = {}
-    # for key, value in bluetooth_data.items():
-    #     if key.startswith("St"):
-    #         modified_bluetooth[key] = value
-    #     elif key.startswith("Eq"):
-    #         # Insert into SQLite database
-    #         c.execute("INSERT INTO Eq_beacons VALUES (?, ?, ?, ?, ?, ?)", 
-    #                   (timestamp, family, device, location, key, value))
-    #         conn.commit()
+                # This block writes headers in the CSV file if it was just created
+                if csvfile.tell() == 0:
+                    writer.writeheader() 
 
-    # data["bluetooth"] = modified_bluetooth
+                # Append data to CSV
+                writer.writerow({'timestamp': timestamp, 'family': family, 'device': device, 'location': location, 'key': key, 'value': value})
+
+    data["bluetooth"] = modified_bluetooth
     return data
+
 
 sensor_data = json.loads(sensors)
 processed_data = process_data(sensor_data)
@@ -42,12 +53,8 @@ processed_data = process_data(sensor_data)
 processed_json = json.dumps(processed_data)
 print(processed_json)
 
-# Close the connection to the SQLite database
-conn.close()
-
-
-
-
+with open('/app/main/static/img2/processed_data.txt', 'w') as f:
+        f.write(processed_json + "\n")
 
 
 
