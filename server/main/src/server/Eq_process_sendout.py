5d430c41c80f32a479af20013c8367a393680855
Nima <naa51@sfu.ca>
2023-07-03 21:47:21 -0700

    Add location in tracking mode for equipment csv

--- server/main/src/server/Eq_process_sendout.py
+++ server/main/src/server/Eq_process_sendout.py
@@ -20,6 +20,32 @@ with open('/app/main/static/img2/eq_process_sendout.txt', 'w') as f:
 csv_filename_eq = '/app/main/static/img2/Eq_beacons.csv'
 csv_filename_wrk = '/app/main/static/img2/Workers_conditions.csv'
 
+# Load Equipment CSV data into memory
+csv_data = []
+try:
+    with open(csv_filename_eq, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            csv_data.append(row)
+except FileNotFoundError:
+    pass
+    # print("File not found")
+
+# Modify the data
+for row in csv_data:
+    # Check if timestamp, family, and device match and location is empty
+    if int(row['timestamp']) == timestamp and row['family'] == family and row['device'] == device and row['location'] == '':
+        # Update the location
+        row['location'] = location
+
+# Write the modified data back to the CSV file
+with open(csv_filename_eq, 'w', newline='') as csvfile:
+    fieldnames = ['timestamp', 'family', 'device', 'location', 'key', 'value']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in csv_data:
+        writer.writerow(row)
+
 # Load worker's last known conditions
 workers_conditions = {}
 try:
@@ -33,7 +59,7 @@ except FileNotFoundError:
 def process_data(location):
     # Extract the bluetooth data
     
-    
+    location = location[:-1] + 'yd'
     return location
 
 
